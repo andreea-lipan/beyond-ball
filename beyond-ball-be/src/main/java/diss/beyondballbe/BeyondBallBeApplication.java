eeb69d78a408465900f50f27713d43c54411ac81
andreea <andreea.alexandra.lipan@gmail.com>
2025-04-05 14:22:04 +0300

    initial commit

---
+++ beyond-ball-be/src/main/java/diss/beyondballbe/BeyondBallBeApplication.java
@@ -0,0 +1,13 @@
+package diss.beyondballbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondBallBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeyondBallBeApplication.class, args);
+    }
+
+}
