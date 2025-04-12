0ac151b8bce09ec3d612e3fadec723f1a2c3b6d8
MariaCuconu <91667648+912-CUCONU-MARIA@users.noreply.github.com>
2025-04-12 17:46:26 +0300

    Updated jwtUtil

--- beyond-ball-be/src/main/java/diss/beyondballbe/security/JwtUtil.java
+++ beyond-ball-be/src/main/java/diss/beyondballbe/security/JwtUtil.java
@@ -4,9 +4,11 @@ import diss.beyondballbe.model.accounts.UserAccount;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -18,6 +20,10 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private long jwtExpirationMs;
 
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
     public String generateToken(UserAccount user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
@@ -26,13 +32,14 @@ public class JwtUtil {
                 .claim("teamId", user.getTeam().getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(jwtSecret)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
