15a0e125b5e68a13410705190179680aff2780af
mitro <mitro.andrei@gmail.com>
2025-05-23 17:10:34 +0300

    player profile

--- beyond-ball-fe/src/pages/profile/PlayerProfileContainer.jsx
+++ beyond-ball-fe/src/pages/profile/PlayerProfileContainer.jsx
@@ -1,9 +1,10 @@
-import {Box, Button, Typography, useTheme} from "@mui/material";
+import {Avatar, Box, Button, Typography, useTheme} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {QuizCard} from "../quizzes/view/QuizCard.jsx";
 import QuizService from "../../APIs/QuizService.js";
 import useModal from "../../components/modals/useModal.js";
 import {UploadAvatarModal} from "./UploadAvatarModal.jsx";
+import {PROFILE_PAGE} from "../../utils/UrlConstants.js";
 
 export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
     const theme = useTheme();
@@ -16,30 +17,24 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
         player && QuizService.getCompletedQuizzesForPlayer(player?.id).then((quizzes) => {
             setQuizzes(quizzes);
         })
-    },[player])
+    }, [player])
 
     return (
-        <>
-            <UploadAvatarModal state={uploadModal} handleConfirm={uploadAvatar} />
+        <Box>
+            <UploadAvatarModal state={uploadModal} handleConfirm={uploadAvatar}/>
 
             {/* Player Information */}
             <Box sx={{
                 padding: "24px 5px 24px 5px",
                 borderRadius: "16px",
-                flex: 1,
+                // flex: 1,
                 display: 'flex',
-                flexDirection: 'row',
+                flexDirection: 'row'
             }}>
                 {/* Player Image */}
-                <Box sx={{
-                    backgroundColor: theme.palette.primary.main,
-                    borderRadius: "16px",
-                    padding: "24px 5px 24px 5px",
-                    flex: 1,
-                    m: 1
-                }}>
-                    <img className="avatar" src={avatar} alt={"avatar"} />
-                    <Button onClick={uploadModal.openModal} variant={"contained"}>Upload Avatar</Button>
+                <Box sx={{m: 'auto', display: 'flex', padding: '0 20px 0 20px'}}>
+                    <Avatar src={avatar} alt={"Profile Image"} sx={{width: 150, height: 150, m: 'auto'}}
+                            onClick={uploadModal.openModal}/>
                 </Box>
 
                 {/* Player Base Information */}
@@ -50,10 +45,39 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
                     flex: 1,
                     m: 1
                 }}>
-                    Dates
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        padding: '0 40px 0 40px'
+                    }}>
+                        <Box sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'flex-start',
+                            paddingRight: '10px'
+                        }}>
+                            <Typography variant="body1">Position:</Typography>
+                            <Typography variant="body1">Nationality:</Typography>
+                            <Typography variant="body1">DOB:</Typography>
+                            <Typography variant="body1">Height:</Typography>
+                            <Typography variant="body1">Weight:</Typography>
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'flex-end',
+                            paddingLeft: '10px'
+                        }}>
+                            <Typography variant="body1">{'Forward'}</Typography>
+                            <Typography variant="body1">{'Romanian'}</Typography>
+                            <Typography variant="body1">{'1995-06-15'}</Typography>
+                            <Typography variant="body1">{'180.5'}</Typography>
+                            <Typography variant="body1">{'75'}</Typography>
+                        </Box>
+                    </Box>
                 </Box>
 
-                {/* Player Stats */}
+                {/* Player Goals */}
                 <Box sx={{
                     backgroundColor: theme.palette.primary.main,
                     borderRadius: "16px",
@@ -61,7 +85,20 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
                     flex: 1,
                     m: 1
                 }}>
-                    Golas
+                    <Typography variant="h1" sx={{fontWeight: 500, color: theme.palette.text.primary}}> Goals </Typography>
+                    <Typography variant="h1" sx={{color: theme.palette.text.primary, p : 1}}> 3 </Typography>
+                </Box>
+
+                {/* Player Assists */}
+                <Box sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: "16px",
+                    padding: "24px 5px 24px 5px",
+                    flex: 1,
+                    m: 1
+                }}>
+                    <Typography variant="h1" sx={{fontWeight: 500, color: theme.palette.text.primary}}> Assists </Typography>
+                    <Typography variant="h1" sx={{color: theme.palette.text.primary, p : 1 }}> 10 </Typography>
                 </Box>
             </Box>
 
@@ -70,6 +107,7 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
                 backgroundColor: theme.palette.primary.main,
                 borderRadius: "16px",
                 padding: "24px 5px 24px 5px",
+                m: "14px"
             }}>
                 <Typography variant="h2" sx={{fontWeight: 700, p: 2}} gutterBottom> Completed Quizzes </Typography>
 
@@ -78,7 +116,7 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
                         display: "flex",
                         flexDirection: "row",
                         gap: "1vw",
-                        mt: 2,
+                        m: 2,
                         flexWrap: "wrap",
                         justifyContent: "center",
                     }}
@@ -92,12 +130,12 @@ export const PlayerProfileContainer = ({player, uploadAvatar, avatar}) => {
                             );
                         })
                         :
-                        <Typography variant="h2" sx={{fontWeight: 700}} gutterBottom> No Quizzes completed yet. Complete
+                        <Typography variant="body1" gutterBottom> No Quizzes completed yet. Complete
                             your first quiz in the Quizzes Page! </Typography>
                     }
 
                 </Box>
             </Box>
-        </>
+        </Box>
     );
 }
